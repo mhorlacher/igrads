741f0f0afca799fe5b4f6540d5b13dc7c19015c9
Marc Horlacher <marc.horlacher@gmail.com>
2021-08-10 13:54:59 +0200

    adjusted figure width and heigh

--- igrads/visualization.py
+++ igrads/visualization.py
@@ -15,10 +15,14 @@ def plot_sequence_attribution(attribution_matrix):
     # style using Logo methods
     attribution_logo.style_spines(visible=False)
     attribution_logo.style_spines(spines=['left', 'bottom'], visible=True)
-    attribution_logo.style_xticks(rotation=90, fmt='%d', anchor=0)
+    #attribution_logo.style_xticks(rotation=90, fmt='%d', anchor=0)
 
     # style using Axes methods
     attribution_logo.ax.set_ylabel("IG Attribution", labelpad=-1)
-    attribution_logo.ax.xaxis.set_tick_params(which='both', bottom=False, top=False, labelbottom=False)
+    #attribution_logo.ax.xaxis.set_tick_params(which='both', bottom=False, top=False, labelbottom=False)
+
+    # adjust figure width and heigh
+    logo.fig.set_figheight(3)
+    logo.fig.set_figwidth(20)
 
     return attribution_logo
