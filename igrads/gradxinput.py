5b18d19134df7fdd0e5205ca70418e4b8d4a93ea
Marc Horlacher <marc.horlacher@gmail.com>
2022-04-26 16:18:58 +0200

    added gradxinput.py

--- igrads/gradxinput.py
+++ igrads/gradxinput.py
@@ -7,4 +7,5 @@ from igrads.igrads import _compute_gradients
 # %%
 @tf.function
 def grad_x_input(inputs, model, target_mask=None, postproc_fn=None):
-    return _compute_gradients(inputs, model, target_mask=None, postproc_fn=None) * inputs
+    inputs = tf.expand_dims(inputs, axis=0)
+    return tf.squeeze(_compute_gradients(inputs, model, target_mask=None, postproc_fn=None) * inputs)
